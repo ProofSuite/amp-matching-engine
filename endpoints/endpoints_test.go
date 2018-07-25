0bfea6c03ec6155d98abe421a6f1eeb06b8dd85a
Aman <aman.sardana880@gnail.com>
2018-07-25 18:45:06 +0530

    basic unit test for token and order socket connection for order test created

--- endpoints/endpoints_test.go
+++ endpoints/endpoints_test.go
@@ -2,25 +2,32 @@ package endpoints
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/Proofsuite/amp-matching-engine/app"
 	"github.com/Proofsuite/amp-matching-engine/daos"
+	"github.com/Proofsuite/amp-matching-engine/ws"
 	"github.com/Sirupsen/logrus"
 	routing "github.com/go-ozzo/ozzo-routing"
 	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/posener/wstest"
 	"github.com/stretchr/testify/assert"
 )
 
 type apiTestCase struct {
-	tag      string
-	method   string
-	url      string
-	body     string
-	status   int
-	response string
+	tag         string
+	method      string
+	url         string
+	body        string
+	status      int
+	response    interface{}
+	checkMethod string
+	compareFn   func(t *testing.T, actual, expected interface{})
 }
 
 func newRouter() *routing.Router {
@@ -50,16 +57,43 @@ func testAPI(router *routing.Router, method, URL, body string) *httptest.Respons
 	req.Header.Set("Content-Type", "application/json")
 	res := httptest.NewRecorder()
 	router.ServeHTTP(res, req)
+	httptest.NewServer(router)
 	return res
 }
+func testSocket(body interface{}) {
+	handler := http.HandlerFunc(ws.ConnectionEndpoint)
+	d := wstest.NewDialer(handler)
+	uri := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/socket"}
 
+	c, _, err := d.Dial(uri.String(), nil)
+	if err != nil {
+		panic(err)
+	}
+	c.WriteJSON(body)
+	var resp interface{}
+	c.ReadJSON(&resp)
+	fmt.Printf("\n%s\n", resp)
+}
 func runAPITests(t *testing.T, router *routing.Router, tests []apiTestCase) {
 	for _, test := range tests {
 		res := testAPI(router, test.method, test.url, test.body)
 		assert.Equal(t, test.status, res.Code, test.tag)
 		if test.response != "" {
-			// assert.JSONEq(t, test.response, res.Body.String(), test.tag)
-			assert.Contains(t, test.response, res.Body.String(), test.tag)
+			var resp interface{}
+			if err := json.Unmarshal(res.Body.Bytes(), &resp); err != nil {
+				fmt.Errorf("%s", err)
+			}
+			switch test.checkMethod {
+			case "contains":
+				assert.Contains(t, resp, test.response, test.tag)
+			case "equals":
+				assert.JSONEq(t, test.response.(string), res.Body.String(), test.tag)
+			case "subset":
+				assert.Subset(t, resp, test.response, test.tag)
+			case "custom":
+				test.compareFn(t, resp, test.response)
+			}
 		}
 	}
 }
+
