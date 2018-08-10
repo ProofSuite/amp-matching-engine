b02ea1d9604e2c420924bc777fa66b70397122fe
Aman <aman.sardana880@gmail.com>
2018-08-10 15:06:50 +0530

    balance nonzero balance queryParam added

--- daos/balance.go
+++ daos/balance.go
@@ -1,6 +1,7 @@
 package daos
 
 import (
+	"encoding/json"
 	"errors"
 	"time"
 
@@ -35,14 +36,56 @@ func (dao *BalanceDao) Create(balance *types.Balance) (err error) {
 
 // GetByAddress function fetches document from db collection based on user address.
 // Returns Balance type struct
-func (dao *BalanceDao) GetByAddress(addr string) (response *types.Balance, err error) {
-	var res []*types.Balance
-	q := bson.M{"address": addr}
-	err = db.Get(dao.dbName, dao.collectionName, q, 0, 1, &res)
+func (dao *BalanceDao) GetByAddress(addr string, nonZeros ...bool) (response *types.Balance, err error) {
+	var q []bson.M
+	nonZero := true
+	if len(nonZeros) > 0 {
+		nonZero = nonZeros[0]
+	}
+	if nonZero {
+		q = []bson.M{bson.M{
+			"$match": bson.M{
+				"address": "0xefD7eB287CeeFCE8256Dd46e25F398acEA7C4b64",
+			},
+		}}
+	} else {
+
+		q = []bson.M{bson.M{
+			"$match": bson.M{
+				"address": "0xefD7eB287CeeFCE8256Dd46e25F398acEA7C4b64",
+			},
+		}, bson.M{
+			"$addFields": bson.M{
+				"tokens": bson.M{
+					"$objectToArray": "$tokens",
+				},
+			},
+		}, bson.M{
+			"$addFields": bson.M{
+				"tokens": bson.M{
+					"$filter": bson.M{
+						"input": "$tokens",
+						"as":    "token",
+						"cond": bson.M{
+							"$ne": []interface{}{"$$token.v.amount", 0},
+						},
+					},
+				},
+			},
+		}, bson.M{
+			"$addFields": bson.M{
+				"tokens": bson.M{
+					"$arrayToObject": "$tokens",
+				},
+			},
+		}}
+	}
+	res, err := db.Aggregate(dao.dbName, dao.collectionName, q)
 	if err != nil {
 		return
 	} else if len(res) > 0 {
-		response = res[0]
+		resAsBytes, _ := json.Marshal(res[0])
+		json.Unmarshal(resAsBytes, &response)
 	} else {
 		err = errors.New("No Wallet for address found")
 	}
