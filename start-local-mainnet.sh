558e911d544cd5c3ca859d2ff9b9cbed2140d001
David Van Isacker <davidvanisacker@gmail.com>
2019-02-25 13:54:10 +0900

    fix infura url

--- start-local-mainnet.sh
+++ start-local-mainnet.sh
@@ -3,7 +3,7 @@ AMP_MONGODB_URL=localhost; \
 AMP_MONGODB_DBNAME=proofdex; \
 AMP_EXCHANGE_CONTRACT_ADDRESS=0x3e74af98042dc0dc75c41731e178cf739c8cc5bb; \
 AMP_ETHEREUM_NODE_HTTP_URL=https://mainnet.infura.io/; \
-AMP_ETHEREUM_NODE_WS_URL=wss://mainnet.infura.io/_ws; \
+AMP_ETHEREUM_NODE_WS_URL=wss://mainnet.infura.io/ws; \
 AMP_ENABLE_TLS=false; \
 fresh
 
